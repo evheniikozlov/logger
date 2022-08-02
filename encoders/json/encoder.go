fe5faf2c044ba665c4cc58379912a3bd2ba17721
evheniikozlov <107479197+evheniikozlov@users.noreply.github.com>
2022-08-02 21:37:03 +0300

    json encoder added

---
+++ encoders/json/encoder.go
@@ -0,0 +1,17 @@
+package txt
+
+import (
+	"encoding/json"
+	"github.com/evheniikozlov/logger"
+)
+
+type Encoder struct {
+}
+
+func (encoder Encoder) Encode(message logger.Message) (string, error) {
+	messageJson, err := json.Marshal(message)
+	if err != nil {
+		return "", err
+	}
+	return string(messageJson), nil
+}
---
+++ encoders/json/go.mod
@@ -0,0 +1,5 @@
+module github.com/evheniikozlov/logger/encoders/txt
+
+go 1.18
+
+require github.com/evheniikozlov/logger v0.0.0-20220802175239-a435c889abbe
